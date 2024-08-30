e204477afce59bf4aed57ec97cdb473055b3c32e
Andrew Newport <anewport@outlook.com>
2024-08-30 14:22:45 -0400

    first successful test transaction

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const axios = require('axios');
 const express = require('express');
 const path = require('path');
 
-
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
@@ -15,44 +14,13 @@ app.get('/', (req, res) => {
     res.send('Hello, HTTPS!');
 });
 
-const ACHRequestTemplate = {
-        "name": "string",
-        "aba": "string",
-        "dda": "string",
-        "accountType": "C",
-        "locationId": "string",
-        "amount": "string",
-        "depositName": "string",
-        "creditOrDebit": "C",
-        "entryClassCode": "CCD",
-        "checkSerialNumber": "string",
-        "addenda": "string",
-        "traceNumber": "string",
-        "discretionaryData": "S",
-        "companyDescription": "string",
-        "terminalCity": "string",
-        "terminalState": "string",
-        "effectiveDate": "string",
-        "accountValidation": "Y",
-        "lineItem": [
-        {
-            "amount": 0,
-            "auxFields": [
-                {
-                    "auxFieldName": "string",
-                    "auxFieldValue": "string"
-                }
-            ]
-        }
-    ]
-}
-
+// The ACH request body without line items
 const ACHTestTemplate = {
     "name": "First Last",
-    "aba": "192837465",
+    "aba": "011000015",  // The ABA number used in the successful curl request
     "dda": "1029384756",
     "accountType": "C",
-    "locationId": "123",
+    "locationId": "5047323",
     "amount": "123.45",
     "depositName": "name of deposit",
     "creditOrDebit": "C",
@@ -65,54 +33,26 @@ const ACHTestTemplate = {
     "terminalCity": "Los Angeles",
     "terminalState": "CA",
     "effectiveDate": "2024-09-01",
-    "accountValidation": "Y",
-    "lineItem": [
-        {
-            "amount": 123.45,
-            "auxFields": [
-                {
-                    "auxFieldName": "name",
-                    "auxFieldValue": "value"
-                }
-            ]
-        }
-    ]
-}
+    "accountValidation": "Y"
+};
 
-// // Add a route to trigger the ACH request
-// app.post('/sendACH', async (req, res) => {
-//     try {
-//         const response = await axios.post('https://www.istreamdeposit.com/tlsrest/sendDetailedACH', ACHTestTemplate, {
-//             headers: {
-//                 'accept': 'application/json',
-//                 'Content-Type': 'application/json'
-//             }
-//         });
-//         // Print the response to the console
-//         console.log(response.data);
-//         // Send a response back to the client
-//         res.send('ACH Request sent! Response: ' + JSON.stringify(response.data));
-//     } catch (error) {
-//         // Handle errors and print them to the console
-//         console.error('Error making the request:', error);
-//         res.status(500).send('Error making the ACH request');
-//     }
-// });
+// Example of adding Basic Authentication
+const username = process.env.ISTREAM_USERNAME;
+const password = process.env.ISTREAM_PASSWORD;
+const authHeader = 'Basic dGVzdGxldHBhbWluYzp3SHZ2enQ9WXY2Q2lucg=='; // TODO: replace hardcoded value with calculated value
 
 app.get('/sendACH', async (req, res) => {
     try {
         const response = await axios.post('https://www.istreamdeposit.com/tlsrest/sendDetailedACH', ACHTestTemplate, {
             headers: {
                 'accept': 'application/json',
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': authHeader
             }
         });
-        // Print the response to the console
         console.log(response.data);
-        // Send a response back to the client
         res.send('ACH Request sent! Response: ' + JSON.stringify(response.data));
     } catch (error) {
-        // Handle errors and print them to the console
         console.error('Error making the request:', error);
         res.status(500).send('Error making the ACH request');
     }
